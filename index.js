3186c50e911ec4aee5adc5105eeb10b1591bdd29
Sarwech Shar <sarwech.shar@gmail.com>
2017-05-29 08:45:39 +0100

    replace concat with literals

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ $( 'button#getwiki' ).click(event => {
 
 function getWiki() {
   // var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&exsentences=2&explaintext=&indexpageids&titles=";
-  const url = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&limit=10&namespace=0&callback=?&search="
+  const url = `https://en.wikipedia.org/w/api.php?format=json&action=opensearch&limit=10&namespace=0&callback=?&search=`
   const query = document.getElementById( 'searchField' ).value;
   console.log(query);
   
@@ -33,23 +33,14 @@ function getWiki() {
     
     if (data[1]["length"] === 0) {
       results = [];
-      results.push(["<li><strong>Nothing was found, try again.</strong></li>"]);
+      results.push([`<li><strong>Nothing was found, try again.</strong></li>`]);
       $(results).appendTo( '#results' ).fadeIn(160);
     } else {
       for (let i = 0; i < 10; i++) {
         const title = data[1][i];
         const summary = data[2][i];
         const link = data[3][i];
-        results.push(["<a href=",
-                     link,
-                     " target='_blank'><li>",
-                     "<strong>",
-                     title,
-                     "</strong>",
-                     "<br/>",
-                     summary,
-                     "<br/>",
-                     "</li></a><br/>"].join(''));
+        results.push(`<a href='${link}' target='_blank'><li><strong>${title}</strong><br/>${summary}<br/></li></a><br/>`);
         $(results[i]).appendTo( '#results' ).fadeIn(160);
       }; 
     }
