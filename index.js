1caf4e0e4baf334005d288d50a8645f09b453990
Sarwech Shar <sarwech.shar@gmail.com>
2017-05-29 08:40:33 +0100

    update JS to ES6 and change button styling

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-$(document).ready(function() {
-  $('body').hide().fadeIn(1000);
+$(document).ready(() => {
+  $( 'body' ).fadeIn(1000);
 });
 
-var searchField = document.getElementById("searchField");
+let searchField = document.getElementById( 'searchField' );
 
-searchField.addEventListener("keydown", function (e) {
+searchField.addEventListener("keydown", e => {
     if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
         getWiki();
     }
 });
 
+$( 'button#getwiki' ).click(event => {
+  event.preventDefault()
+  getWiki()
+})
+
 function getWiki() {
   // var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&exsentences=2&explaintext=&indexpageids&titles=";
-  var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=opensearch&limit=10&namespace=0&search="
-  var query = document.getElementById("searchField").value;
+  const url = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&limit=10&namespace=0&callback=?&search="
+  const query = document.getElementById( 'searchField' ).value;
   console.log(query);
   
-  $( ".results" ).empty();
+  $( '#results' ).empty();
   
-  $.getJSON(url + query, function(data) {
+  $.getJSON(url + query, data => {
     // var pageid = data.query.pageids[0];
     // console.log(pageid)
     // result = data.query.pages[pageid].extract;
-    var results = [];
-
-    for (var i = 0; i < 10; i++) {
-      var title = data[1][i];
-      var summary = data[2][i];
-      var link = data[3][i];
-      results.push("<a href=" + link + " target='_blank'><li>" + "<strong>" + title + "</strong>" + "<br/>" + summary + "<br/>" + "</li></a><br/>");
-      $(results[i]).hide().appendTo("#results").fadeIn(1000);
-    };
-    $('.results').hide().fadeIn(1000);
+    console.log(data[1]["length"]);
+    console.log(data)
+    let results = [];
+    
+    if (data[1]["length"] === 0) {
+      results = [];
+      results.push(["<li><strong>Nothing was found, try again.</strong></li>"]);
+      $(results).appendTo( '#results' ).fadeIn(160);
+    } else {
+      for (let i = 0; i < 10; i++) {
+        const title = data[1][i];
+        const summary = data[2][i];
+        const link = data[3][i];
+        results.push(["<a href=",
+                     link,
+                     " target='_blank'><li>",
+                     "<strong>",
+                     title,
+                     "</strong>",
+                     "<br/>",
+                     summary,
+                     "<br/>",
+                     "</li></a><br/>"].join(''));
+        $(results[i]).appendTo( '#results' ).fadeIn(160);
+      }; 
+    }
+    $( '#results' ).fadeIn(160);
       console.log(results);
   });
 };
